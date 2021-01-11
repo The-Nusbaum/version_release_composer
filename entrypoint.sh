c8e5f75b130a59f49da7d2456628a84bc2f685ce
The-Nusbaum <38957691+The-Nusbaum@users.noreply.github.com>
2021-01-11 16:59:08 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,7 +39,6 @@ function set_release_notes {
 
 function create_git_tag_and_release {
   # POST a release to repo via Github API
-  echo $REPO_OWNER $repo
   curl -s -X POST https://api.github.com/repos/$REPO_OWNER/$repo/releases \
   -H "Authorization: token $TOKEN" \
   -d @- << EOF
