1f59f5507c42f3882cccb54ae46e1915922d6394
The-Nusbaum <38957691+The-Nusbaum@users.noreply.github.com>
2021-01-12 15:17:06 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-function get_latest_tag {
-  describe_tag || tail_tag
-  echo "Latest Tag:" $latest_tag
-}
-
-function describe_tag {
-  latest_tag=$(git describe --abbrev=0 --tags)
-}
-
-function tail_tag {
-  latest_tag=$(git tag | tail -1)
+function get_files {
+  files= $(git --no-pager diff --name-only FETCH_HEAD)
 }
 
 function get_current_info {
@@ -19,45 +10,26 @@ function get_current_info {
   echo "Trigger:" $current_branch
 }
 
-function prepare_file_info {
-  version_on_file=$(cut -d " " -f 1 $file_path)
-  if [[ $version_on_file == v* ]]; then version_on_file=$(echo $version_on_file | cut -c2-); fi
-  echo "Version on file:" $version_on_file
-}
-
 function prepare_github_info {
   remote=$(git config --get remote.origin.url)
   repo=$(basename $remote .git)
 }
 
-function set_release_notes {
-  release_notes=$(git log $latest_tag..HEAD --merges --pretty=tformat:"%h %s <br/>")
-  # Checking if the release notes are empty to get individual commits instead
-  if [ -z $release_notes ]; then release_notes=$(git log $latest_tag..HEAD --pretty=tformat:"%h %s <br/>"); fi
-  echo "Release Notes:" $release_notes
-}
-
-function create_git_tag_and_release {
-  # POST a release to repo via Github API
-  curl -s -X POST https://api.github.com/repos/$REPO_OWNER/$repo/releases \
-  -H "Authorization: token $TOKEN" \
-  -d @- << EOF
-  {
-  "tag_name": "$PREPEND$version_on_file$APPEND",
-  "target_commitish": "$current_branch",
-  "name": "Release $version_on_file",
-  "body": "$release_notes",
-  "draft": $DRAFT,
-  "prerelease": $PRERELEASE
-  }
-EOF
-}
-
-function validate_version_file {
-  if [ -z "$VERSION_FILE_NAME" ]; then echo "VERSION_FILE_NAME is missing from your configuration"; exit $?; fi
-  file_path=$(find . -name $VERSION_FILE_NAME)
-  if [ -z "$file_path" ]; then echo "Cannot locate version file"; exit $?; fi
-}
+# function create_git_tag_and_release {
+#   # POST a release to repo via Github API
+#   curl -s -X POST https://api.github.com/repos/$REPO_OWNER/$repo/releases \
+#   -H "Authorization: token $TOKEN" \
+#   -d @- << EOF
+#   {
+#   "tag_name": "$PREPEND$version_on_file$APPEND",
+#   "target_commitish": "$current_branch",
+#   "name": "Release $version_on_file",
+#   "body": "$release_notes",
+#   "draft": $DRAFT,
+#   "prerelease": $PRERELEASE
+#   }
+# EOF
+# }
 
 cd $GITHUB_WORKSPACE/
 
@@ -65,24 +37,22 @@ echo "------------- Script Starting ----------------------"
 
 git fetch --prune-tags
 
-validate_version_file
-get_latest_tag
-get_current_info
+get_files
 
-files=$(git diff --name-status $latest_tag HEAD | grep $VERSION_FILE_NAME)
+echo $files
 
-if [ -z "$files" ];
-then
-  echo "Nothing to tag!";
-  exit $?
-else
-  echo "Version File has been updated, proceeding to tag"
-  prepare_file_info
-  prepare_github_info
-  set_release_notes
-  create_git_tag_and_release
-  result=$(create_git_tag_and_release)
-  echo $result | jq .url
-  exit $?
-fi
+# if [ -z "$files" ];
+# then
+#   echo "Nothing to tag!";
+#   exit $?
+# else
+#   echo "Version File has been updated, proceeding to tag"
+#   prepare_file_info
+#   prepare_github_info
+#   set_release_notes
+#   create_git_tag_and_release
+#   result=$(create_git_tag_and_release)
+#   echo $result | jq .url
+#   exit $?
+# fi
 echo "------------- Script Ending ----------------------"
